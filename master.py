2f0fee976f7b9feb6f41d905016209d069e9391e
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-12 17:38:59 +0300

    Create Game Loop Add bg_color Add screen

--- master.py
+++ master.py
@@ -1,1 +1,27 @@
+import pygame
+import sys
 
+pygame.init()
+
+SCREEN_WIDTH = 750
+SCREEN_HEIGHT = 700
+
+GREY = (29, 29, 27)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Baelrin's Space Invaders")
+
+clock = pygame.time.Clock()
+
+while True:
+    # Checking for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Drawing
+    screen.fill(GREY)
+
+    pygame.display.update()
+    clock.tick(60)
