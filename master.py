2332ec55e1a64f04c16ce734841c6d6f7ed072f0
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-13 18:43:03 +0300

    Create Aliens

--- master.py
+++ master.py
@@ -16,15 +16,22 @@ clock = pygame.time.Clock()
 
 game = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+SHOOT_LASER = pygame.USEREVENT
+pygame.time.set_timer(SHOOT_LASER, 300)
+
 while True:
     # Checking for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SHOOT_LASER:
+            game.alien_shoot_laser()
 
     # Updating
     game.spaceship_group.update()
+    game.move_aliens()
+    game.alien_lasers_group.update()
 
     # Drawing
     screen.fill(GREY)
@@ -32,6 +39,8 @@ while True:
     game.spaceship_group.sprite.lasers_group.draw(screen)
     for obstacle in game.obstacles:
         obstacle.blocks_group.draw(screen)
+    game.aliens_group.draw(screen)
+    game.alien_lasers_group.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
