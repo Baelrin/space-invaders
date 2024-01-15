3ef087b84556e31aaf4ad4e1f4fc6b7415b5de71
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-15 18:14:58 +0300

    Fix None-type errors by Pylance

--- alien.py
+++ alien.py
@@ -21,18 +21,18 @@ class MysteryShip(pygame.sprite.Sprite):
         self.screen_width = screen_width
         self.offset = offset
         self.image = pygame.image.load('Graphics/mystery.png')
-        
+
         x = random.choice([self.offset/2, self.screen_width + self.offset - self.image.get_width()])
-        if x == self.offset/2:
-            self.speed = 3
-        else:
-            self.speed = -3
-        
+        self.speed = 3 if x == self.offset/2 else -3
         self.rect = self.image.get_rect(topleft = (x, 90))
         
     def update(self):
-        self.rect.x += self.speed
-        if self.rect.right > self.screen_width + self.offset/2:
-            self.kill()
-        elif self.rect.left < self.offset/2:
-            self.kill()
+        if self.rect is not None:
+            self.rect.x += self.speed
+        else: 
+            print('Error: self.rect is None')
+        if self.rect is not None:
+            if self.rect.right > self.screen_width + self.offset / 2:
+                self.kill()
+            elif self.rect.left < self.offset / 2:
+                self.kill()
